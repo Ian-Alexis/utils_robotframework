030d70a380147c38cc70d0cf77c7bc2a0fb287e4
Ian Hang <Ian.Hang@covage.com>
2023-07-12 16:51:47 +0200

    adding filter testing

--- front/anyway_playwright.py
+++ front/anyway_playwright.py
@@ -5,6 +5,8 @@ import json
 import logging 
 from logging import getLogger
 
+url = "https://anyway.qal.covage.com/dashboard"
+
 def Erase_if_exist(page, constructeur_text):
     page.get_by_placeholder("Rechercher...").nth(0).click()
     page.get_by_placeholder("Rechercher...").nth(0).fill(constructeur_text)
@@ -41,17 +43,17 @@ def get_column_content(page, column_name):
 
     # Get the column content
     rows = page.query_selector_all('tr')
-    rows.pop(0) # On enblève le header 
+    rows.pop(0) # On enlève le header 
     column_content = []
     for row in rows:
         # print(rows[0].text_content().strip())
         cells = row.query_selector_all('td')
-        column_content.append(cells[column_index].text_content().strip())
+        column_content.append(cells[column_index].text_content().strip().lower())
 
     column_content_clean = [element.replace('\n','') for element in column_content]
     column_content_clean.pop(0) # On enlève l'élément provenant du bandeau rechercher
 
-    print("\n",column_content_clean)
+    # print("\n",column_content_clean)
 
     return column_content_clean
 
@@ -61,3 +63,108 @@ def select_scrolling_menu(page, menu_name, option_text):
     
     # Click on the option with the given text
     page.get_by_text(option_text).nth(1).click()
+
+def Filter_try():
+    with sync_playwright() as PS:
+        # setup test
+        browser = PS.chromium.launch(headless=False)
+        context = browser.new_context()
+        context.tracing.start(screenshots=True, snapshots=True, sources=True)
+        page = context.new_page()
+        # go to anyway
+        page.goto(url)
+        filter_all_search_test(page)
+        # save recording in a zip file
+        context.tracing.stop(path="out/trace.zip")
+        # to close browser
+        context.close()
+        browser.close()
+    return True
+
+
+def filter_all_search_test(page,  menu=None, tab=None):
+    # extract tabs json file
+    with open("data/tabs.json", "r", encoding="utf-8") as file_tabs:
+        data_tabs = json.load(file_tabs)
+    for tabs in data_tabs:
+        page.goto(data_tabs[tabs][2])
+        # filter_search_test(page, tabs)
+        test_whole_tab(page,tabs)
+
+
+def filter_search_test(page, tab, test=None):
+    # extract json file
+    with open("data/{}.json".format(tab), "r", encoding="utf-8") as file:
+        data_tab = json.load(file)
+    # extract name of column
+    page.wait_for_selector('//thead[@class="table-dark"]')
+    headers = page.query_selector_all('th')
+    # find all span
+    headers_data = []
+    for header in headers:
+        headers_data.append(header.text_content().strip())
+    headers_data.pop(0)
+    print(headers_data)
+    for header in headers_data:
+        search = data_tab[header]
+        index = find_index(headers_data, header) + 2
+        xpath = '//tbody/tr/td[position()={}]/div'.format(index)
+        element = page.wait_for_selector(xpath)
+        page.click(xpath)
+        try:
+            if "input-group" in element.get_attribute("class"):
+                el = "/input"
+                xpath_input = '//tbody/tr/td[position()={}]/div{}'.format(index, el)
+                element_input = page.wait_for_selector(xpath_input)
+                element_input.fill(search)
+        except:
+            option = '//li/a/span[contains(text(),"{}")]'.format(search)
+            page.click(option)
+
+def find_index(liste, el):
+    index = 0
+    for i, element in enumerate(liste):
+        if element == el:
+            index = i
+    return index
+
+def test_whole_tab(page, tab, test=None):
+    # extract json file
+    with open("data/{}.json".format(tab), "r", encoding="utf-8") as file:
+        data_tab = json.load(file)
+    # extract name of column
+    page.wait_for_selector('//thead[@class="table-dark"]')
+    headers = page.query_selector_all('th')
+    # find all span
+    headers_data = []
+    for header in headers:
+        headers_data.append(header.text_content().strip())
+    headers_data.pop(0)
+    print(headers_data)
+    for header in headers_data:
+        unfiltered = get_column_content(page,header)
+        unfiltered.sort()
+        print("\n",unfiltered)
+        page.get_by_role("columnheader", name=header).get_by_role("img").click()
+        filtered = get_column_content(page,header)
+        print(filtered,"\n")
+        if filtered == unfiltered:
+            print(header,"filter does work correctly")
+        else:
+            print(header,"filter doesn't work")
+            
+    for header in headers_data:
+        search = data_tab[header]
+        index = find_index(headers_data, header) + 2
+        xpath = '//tbody/tr/td[position()={}]/div'.format(index)
+        element = page.wait_for_selector(xpath)
+        page.click(xpath)
+        try:
+            if "input-group" in element.get_attribute("class"):
+                el = "/input"
+                xpath_input = '//tbody/tr/td[position()={}]/div{}'.format(index, el)
+                element_input = page.wait_for_selector(xpath_input)
+                element_input.fill(search)
+        except:
+            option = '//li/a/span[contains(text(),"{}")]'.format(search)
+            page.click(option)
--- test_transceiver.py
+++ test_transceiver.py
@@ -8,14 +8,53 @@ from logging import getLogger
 import front.anyway_playwright as util
 # playwright codegen
 
+url = "https://anyway.qal.covage.com/"
+constructeur_text_1 = "Constucteur test playwright 1"
+constructeur_text_2 = "Constucteur test playwright 2"
 commande = "playwright show-trace out/trace.zip"
 
+
 def test():
-    if run():
+    if util.Filter_try():
         os.system(commande)
         assert True
     else:
         assert False
+
+def First_try():
+    with sync_playwright() as PS:
+        # setup test
+        browser = PS.chromium.launch(headless=False)
+        context = browser.new_context()
+        context.tracing.start(screenshots=True, snapshots=True, sources=True)
+        page = context.new_page()
+        # go to anyway
+        page.goto(url)
+        page.get_by_role("link", name=" Constructeurs").click()
+        # test if constructor exist
+        util.Erase_if_exist(page, constructeur_text_1)
+        util.Erase_if_exist(page, constructeur_text_2)
+        # create constructor 1
+        page.get_by_role("link", name="").click()
+        page.get_by_label("Nom *").click()
+        page.get_by_label("Nom *").fill(constructeur_text_1)
+        page.get_by_role("button", name="Ajouter").nth(0).click()
+        page.get_by_role("button", name="OK").click()
+        # return on list ok constructeur
+        page.get_by_role("link", name="Constructeurs", exact=True).click()
+        # create constructeur 1 again
+        page.get_by_role("link", name="").click()
+        page.get_by_label("Nom *").click()
+        page.get_by_label("Nom *").fill(constructeur_text_1)
+        page.get_by_role("button", name="Ajouter").nth(1).click()
+        # verify text error
+        text = page.wait_for_selector(".form-text.text-danger").text_content()
+        # save recording in a zip file
+        context.tracing.stop(path="out/trace.zip")
+        # to close browser
+        context.close()
+        browser.close()
+    return True
 
 def run():
     try:
@@ -82,6 +121,8 @@ def run():
 
         page.get_by_role("columnheader", name="Type").get_by_role("img").click()
         util.get_column_content(page,"Type")
+
+        util.filter_search_test(page, "data/tabs")
 
         page.locator("#table-filter-name").click()
         page.locator("#table-filter-name").fill("Transceiver numéro 1")
